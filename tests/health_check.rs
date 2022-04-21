09395157fafd6542cf00301a252f7a491d91e218
ap_smart_st <perisic.ante@gmail.com>
2022-04-21 11:22:45 +0200

    add health_api_and_test_formated

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -3,8 +3,8 @@ mod tests {
     use crate::health_check;
 
     #[tokio::tests]
-    async fn health_check_succeeds(){
-        let response  = health_check().await;
+    async fn health_check_succeeds() {
+        let response = health_check().await;
 
         assert!(response.status().is_ok())
     }
