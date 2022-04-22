b33ed6055fdaf9712ec73cf2f97d2bc6c7175e5f
ap_smart_st <perisic.ante@gmail.com>
2022-04-22 10:54:39 +0200

    run fmt

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -14,13 +14,13 @@ fn spawn_app() -> String {
 
 #[tokio::test]
 async fn health_check_works() {
-    let address  = spawn_app();
+    let address = spawn_app();
 
     let client = reqwest::Client::new();
 
     //Act
     let response = client
-        .get(&format!("{}/health_check",&address))
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -29,5 +29,3 @@ async fn health_check_works() {
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
-
-
