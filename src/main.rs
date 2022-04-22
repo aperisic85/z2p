fb6e106819cd46b8d05062d9e6420861e4d412af
ap_smart_st <perisic.ante@gmail.com>
2022-04-22 10:44:52 +0200

    add test adn radn port

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 //! main.rs
 
+use std::net::TcpListener;
 use z2p::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,13 +1,33 @@
 //! tests/health_check.rs
+use std::net::TcpListener;
+use z2p::run;
 
-use z2p::main;
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1")?;
+    //get port assigned by OS
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address.");
+    let _ = tokio::spawn(server);
+
+    format!("https://127.0.0.1:{}", port);
+}
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let address  = spawn_app();
+
+    let client = reqwest::Client::new();
+
+    //Act
+    let response = client
+        .get(&format!("{}/health_check",&address))
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    //Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> std::io::Result<()> {
-    let server = z2p::run().expect("Failed to bind address");
-    let _ = tokio::spawn(server);
-}
+
